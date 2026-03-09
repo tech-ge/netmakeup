f5db32a72312afb8332f0d323b5b3dde32ff3a5d
tech-ge <geoffreymaitha044@gmail.com>
2026-03-09 14:02:53 -0500

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -4,7 +4,7 @@ const NotificationSchema = new mongoose.Schema({
   userId:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   type:    {
     type: String,
-    enum: ['task_approved','task_rejected','withdrawal_paid','withdrawal_rejected','system'],
+    enum: ['task_approved','task_rejected','withdrawal_paid','withdrawal_rejected','deposit_confirmed','system'],
     required: true
   },
   title:   { type: String, required: true },
